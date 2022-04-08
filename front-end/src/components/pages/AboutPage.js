1a750988a1ce6d95fa0063fabaafcd875b18a14d
ichirodev <adanschiffer@hotmail.com>
2022-04-08 12:36:18 -0500

    Minor changes

--- front-end/src/components/pages/AboutPage.js
+++ front-end/src/components/pages/AboutPage.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import Navbar from '../Navbar'
-import About from '../About'
+import React, { Component } from "react";
+import Navbar from "../Navbar";
+import About from "../About";
 
 export default class AboutPage extends Component {
   render() {
     return (
-        <>
-            <Navbar />
-            <About />
-        </>
-    )
+      <>
+        <Navbar />
+        <About />
+      </>
+    );
   }
 }
