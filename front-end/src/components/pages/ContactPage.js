2a523e093ac6ecc161de6c8c6b3d956d29c4bea6
ichirodev <adanschiffer@hotmail.com>
2022-04-28 18:05:19 -0500

    Contact page

--- front-end/src/components/pages/ContactPage.js
+++ front-end/src/components/pages/ContactPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import ContactMe from "../ContactMe";
 
 export default class ContactPage extends Component {
   render() {
     return (
       <>
         <Navbar />
+        <ContactMe />
         <Footer />
       </>
     );
