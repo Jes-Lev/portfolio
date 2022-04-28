55de7d72e7fb69c0df6f4648045938a3c88c2c79
ichirodev <adanschiffer@hotmail.com>
2022-04-28 15:35:46 -0500

    About page finished (No icons added yet)

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import "./About.css";
-import MyPicture from "./imgs/pic.jpeg";
+import MyPicture from "./imgs/pic.jpg";
 import {
   FaGithub,
-  FaTwitter,
+  FaHackerrank,
   FaLinkedinIn,
   FaUnity,
   FaTerminal,
   FaJsSquare,
 } from "react-icons/fa";
+
+import { MdLocationOn } from "react-icons/md";
+import { IoSchoolSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -20,13 +24,25 @@ const About = () => {
       {/* Social media and others icons */}
       <div className="about-connect-with-me-container">
         <div className="about-connect-with-me">
-          <FaGithub size={"20px"} />
+          <a href="https://github.com/ichirodev" className="about-cwm-icon">
+            <FaGithub size={"20px"} />
+          </a>
         </div>
         <div className="about-connect-with-me">
-          <FaTwitter size={"20px"} />
+          <a
+            href="https://www.hackerrank.com/adanschiffer"
+            className="about-cwm-icon"
+          >
+            <FaHackerrank size={"20px"} />
+          </a>
         </div>
         <div className="about-connect-with-me">
-          <FaLinkedinIn size={"20px"} />
+          <a
+            href="https://www.linkedin.com/in/adan-fregoso-luqu%C3%ADn-6207a3235/"
+            className="about-cwm-icon"
+          >
+            <FaLinkedinIn size={"20px"} />
+          </a>
         </div>
       </div>
       {/* About me header */}
@@ -37,43 +53,31 @@ const About = () => {
       <div className="textholder">
         <div className="textholder-child-box">
           <div className="xtext">
-            Nobis eos eum illum possimus velit non laboriosam sit. Eum sint nam
-            consequatur corrupti eos illum saepe odit. Eligendi delectus dolorem
-            maxime. Possimus ducimus sed qui accusamus voluptatem minima.
-            Commodi delectus consectetur facere facilis aut accusamus placeat
-            et. Ut molestias vel ullam modi corporis. Fugit neque non ea aut
-            quia quia soluta. Vitae repellendus nostrum natus minus earum
-            adipisci cupiditate vel. Ipsum ullam ipsa ex vitae adipisci ab. Sit
-            ipsa qui laudantium autem illum ea nisi. Temporibus rerum quibusdam
-            laborum soluta cupiditate et velit velit. Quo ut sit dolor dolores
-            quia. Voluptatem nisi vitae aut.
+            Soy Adan, estudiante de Ingenieria en Computación apasionado por la
+            tecnología, la ciencia y el arte. Me gusta resolver problemas y
+            desarrollar todo tipo de software, siempre estoy en busca de
+            aprender y aplicar mis conocimientos para crear e innovar.
           </div>
         </div>
       </div>
 
-      {/* Attributes container*/}
-      <div className="attributes-topcontainer">
-        <div className="attributes">
-          {/* Container title */}
-          <div className="attributes-title">Cualidades</div>
+      {/* more container*/}
+      <div className="more-topcontainer">
+        <div className="more">
           {/* Career item boxes, should've used a list... */}
-          <div className="attributes-box">
-            <div className="attributes-icon">Icon</div>
-            <div className="attributes-text">
-              TextasNobis eos eum illum possimus velit non laboriosam sit. Eum
-              sint nam consequatur corruNobis eos eum illum possimus velit non
-              laboriosam sit. Eum sint nam consequatur corru
+          <div className="more-box">
+            <div className="more-icon">
+              <IoSchoolSharp size={"24px"} />
+            </div>
+            <div className="more-text">
+              Ingenieria en Computación, CUCEI, 2018 - actual
             </div>
           </div>
-          <div className="attributes-box">
-            <div className="attributes-icon">Icon</div>
-            <div className="attributes-text">
-              Duis vulputate, metus eu vehicula laoreet, libero odio pretium
-              turpis, sit amet fermentum dolor turpis vel nulla. Praesent
-              posuere volutpat odio. Sed gravida augue urna, vel convallis leo
-              rutrum non. Aenean faucibus nunc erat, et volutpat massa consequat
-              ac. Sed gravida varius feugiat. Donec facilisis finibus interdum.
+          <div className="more-box">
+            <div className="more-icon">
+              <MdLocationOn size={"24px"} />
             </div>
+            <div className="more-text">Guadalajara, Jalisco, MX</div>
           </div>
         </div>
       </div>
@@ -98,8 +102,8 @@ const About = () => {
             <div className="myskillset-skill">MongoDB</div>
             <div className="myskillset-skill">Postgresql</div>
             <div className="myskillset-skill">Unity Engine</div>
-            <div className="myskillset-skill">3D Modelling</div>
             <div className="myskillset-skill">Graphic Design</div>
+            <div className="myskillset-skill">3D Modelling</div>
           </div>
         </div>
       </div>
@@ -111,16 +115,22 @@ const About = () => {
         <div className="textholder">
           <div className="textholder-child-box">
             <div className="xtext">
-              Nobis eos eum illum possimus velit non laboriosam sit. Eum sint
-              nam consequatur corrupti eos illum saepe odit. Eligendi delectus
-              dolorem maxime. Possimus ducimus sed qui accusamus voluptatem
-              minima. Commodi delectus consectetur facere facilis aut accusamus
-              placeat et. Ut molestias vel ullam modi corporis. Fugit neque non
-              ea aut quia quia soluta. Vitae repellendus nostrum natus minus
-              earum adipisci cupiditate vel. Ipsum ullam ipsa ex vitae adipisci
-              ab. Sit ipsa qui laudantium autem illum ea nisi. Temporibus rerum
-              quibusdam laborum soluta cupiditate et velit velit. Quo ut sit
-              dolor dolores quia. Voluptatem nisi vitae aut.
+              Actualmente me encuentro en busca de oportunidades de trabajo o
+              internships en las areas de desarrollo de soluciones de software,
+              desarrollo web o desarrollo de videojuegos.
+              <br></br>
+              Para contactarme puedes enviarme un correo a{" "}
+              <a href="mailto:upsidedownengineer@gmail.com" className="xtext-a">
+                upsidedownengineer@gmail.com
+              </a>
+              <br></br>
+              Puedes descargar mi curriculum{" "}
+              <a
+                className="xtext-a"
+                href="https://drive.google.com/file/d/1dDWd5evUYqWAowKLgsnncL1Kb4ATsOcA/view?usp=sharing"
+              >
+                presionando aquí
+              </a>
             </div>
           </div>
         </div>
